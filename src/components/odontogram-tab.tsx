a8de0d7f3a25391b5adcf5ad661c0962d73cdec1
elnels <nelsonripoll@gmail.com>
2026-07-04 16:51:57 -0600

    fix: add mb-6 spacing to odontogram grid container

--- src/components/odontogram-tab.tsx
+++ src/components/odontogram-tab.tsx
@@ -92,7 +92,7 @@ export function OdontogramTab({ patientId, onDataUpdate }: OdontogramTabProps) {
           </CardTitle>
         </CardHeader>
         <CardContent>
-          <div className="grid grid-cols-1 lg:grid-cols-3 gap-4">
+           <div className="mb-6 grid grid-cols-1 lg:grid-cols-3 gap-4">
             <div className="lg:col-span-2 border rounded-lg p-4">
               <Odontogram
                 teeth={teeth}
